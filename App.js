e374e4d201c625cd93dbf630120f2a0dfc9c3b0a
Rogier Knoester <knoesterrogier@gmail.com>
2018-01-08 13:33:02 +0100

    elements

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Calendar from "./Calendar";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
           <Text>Live reload</Text>
+          <Calendar/>
       </View>
     );
   }
